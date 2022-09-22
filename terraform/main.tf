4eec9161d154ece3bf534d80538d626bd7f7c8bc
Nicolas Lagier <nicolas.lagier@gmail.com>
2022-09-22 17:42:48 +0800

    Terraform module

---
+++ terraform/main.tf
@@ -0,0 +1,55 @@
+
+module "terraform_bucket" {
+  source     = "../s3"
+  name       = "terraform"
+  log_bucket = var.log_bucket
+}
+
+resource "aws_dynamodb_table" "terraform" {
+  name         = "terraform-${terraform.workspace}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  server_side_encryption {
+    enabled = true
+  }
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+}
+
+resource "aws_iam_policy" "terraform" {
+  name_prefix = "terraform-${terraform.workspace}"
+  policy      = <<-POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": "s3:ListBucket",
+        "Resource": "arn:aws:s3:::${module.terraform_bucket.bucket_name}"
+      },
+      {
+        "Effect": "Allow",
+        "Action": ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
+        "Resource": "arn:aws:s3:::${module.terraform_bucket.bucket_name}/*"
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem"
+        ],
+        "Resource": "${aws_dynamodb_table.terraform.arn}"
+      }
+    ]
+  }
+  POLICY
+}
