ef5078f6860bb3ee7cb29ab5d1b2bc5dbdd5b075
Nicolas Lagier <nicolas.lagier@gmail.com>
2023-02-08 13:54:12 +0800

    make aws dependency more linient

--- s3/main.tf
+++ s3/main.tf
@@ -2,12 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.31.0"
+      version = "=> 4.31.0"
     }
   }
 }
-# data "aws_region" "current" {}
-# data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "bucket" {
   count         = var.enabled ? 1 : 0
