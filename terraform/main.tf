d244455769e4d67860a514a68a9f4051904854f4
Nicolas Lagier <nicolas.lagier@gmail.com>
2023-02-11 15:58:50 +0800

    Get terraform module to create its deployment user

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,7 @@ module "terraform_bucket" {
   source     = "../s3"
   name       = "terraform"
   log_bucket = var.log_bucket
+  tags       = var.tags
 }
 
 resource "aws_dynamodb_table" "terraform" {
@@ -11,6 +12,7 @@ resource "aws_dynamodb_table" "terraform" {
   name         = "terraform-${terraform.workspace}"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
+  tags         = var.tags
 
   server_side_encryption {
     enabled = true
@@ -29,6 +31,7 @@ resource "aws_dynamodb_table" "terraform" {
 resource "aws_iam_policy" "terraform" {
   count       = var.enabled ? 1 : 0
   name_prefix = "terraform-${terraform.workspace}"
+  tags        = var.tags
   policy      = <<-POLICY
   {
     "Version": "2012-10-17",
@@ -55,4 +58,30 @@ resource "aws_iam_policy" "terraform" {
     ]
   }
   POLICY
+}
+
+resource "aws_iam_user" "user" {
+  count = var.enabled ? 1 : 0
+  name  = "terraform-backend"
+  path  = "/technical/"
+  tags  = var.tags
+}
+
+resource "aws_iam_group" "group" {
+  count = var.enabled ? 1 : 0
+  name  = "terraform-backend"
+  path  = "/technical/"
+}
+
+resource "aws_iam_group_membership" "group" {
+  count = var.enabled ? 1 : 0
+  name  = "terraform-backend"
+  users = [aws_iam_user.user[0].name]
+  group = aws_iam_group.group[0].name
+}
+
+resource "aws_iam_group_policy_attachment" "group-policy" {
+  count      = var.enabled ? 1 : 0
+  group      = aws_iam_group.group[0].name
+  policy_arn = aws_iam_policy.terraform[0].arn
 }
