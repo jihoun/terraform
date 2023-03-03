63c97838a04d42f6726648ddd57c25fb644dd01d
Nicolas Lagier <nicolas.lagier@gmail.com>
2023-03-03 18:19:50 +0800

    dynamodb module

---
+++ dynamodb_lambda/inputs.tf
@@ -0,0 +1,15 @@
+variable "stream_arn" {
+  type = string
+}
+
+variable "function_name" {
+  type = string
+}
+
+variable "function_role_name" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
---
+++ dynamodb_lambda/main.tf
@@ -0,0 +1,34 @@
+data "aws_iam_policy_document" "policy" {
+  statement {
+    actions = [
+      "dynamodb:GetRecords",
+      "dynamodb:GetShardIterator",
+      "dynamodb:DescribeStream",
+      "dynamodb:ListStreams"
+    ]
+    resources = [var.stream_arn]
+  }
+}
+
+resource "aws_iam_policy" "policy" {
+  path        = "/lambda/${terraform.workspace}/"
+  name_prefix = "${var.function_name}_stream_"
+  policy      = data.aws_iam_policy_document.policy.json
+  tags        = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "role_policy" {
+  role       = var.function_role_name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+resource "aws_lambda_event_source_mapping" "stream_2_lambda" {
+  event_source_arn                   = var.stream_arn
+  function_name                      = var.function_name
+  depends_on                         = [aws_iam_role_policy_attachment.role_policy]
+  maximum_batching_window_in_seconds = 3   #minimizes number of lambda calls
+  batch_size                         = 100 # default dynamodb
+  function_response_types            = ["ReportBatchItemFailures"]
+  starting_position                  = "LATEST"
+  bisect_batch_on_function_error     = true
+}
