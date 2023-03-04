94a5fdae44931117b86c64d4d2004efde9495687
Nicolas Lagier <nicolas.lagier@gmail.com>
2023-03-04 17:06:33 +0800

    Add domain name as an output of cdn

--- cdn/outputs.tf
+++ cdn/outputs.tf
@@ -9,3 +9,7 @@ output "bucket_name" {
 output "bucket_arn" {
   value = module.bucket.bucket_arn
 }
+
+output "domain_name" {
+  value = var.enabled ? "https://${aws_cloudfront_distribution.cloudfront[0].domain_name}" : ""
+}
