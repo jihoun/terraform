6e4cd5d54349d910559d3e2a919ea4bd26c39ea9
Nicolas Lagier <nicolas.lagier@gmail.com>
2023-03-09 07:18:46 +0800

    module to listen to s3 events

--- s3_lambda/main.tf
+++ s3_lambda/main.tf
@@ -1,9 +1,9 @@
 resource "aws_lambda_permission" "allow_bucket" {
-  statement_id  = "AllowExecutionFromS3Bucket"
-  action        = "lambda:InvokeFunction"
-  function_name = var.function_arn
-  principal     = "s3.amazonaws.com"
-  source_arn    = var.bucket_arn
+  statement_id_prefix = "AllowExecutionFromS3Bucket"
+  action              = "lambda:InvokeFunction"
+  function_name       = var.function_arn
+  principal           = "s3.amazonaws.com"
+  source_arn          = var.bucket_arn
 }
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
