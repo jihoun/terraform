06ab9c1e2deb0bc1367fad9f9a0203926728d4e0
Nicolas Lagier <nicolas.lagier@gmail.com>
2023-03-09 07:18:02 +0800

    module to listen to s3 events

---
+++ s3_lambda/inputs.tf
@@ -0,0 +1,26 @@
+variable "function_arn" {
+  type = string
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "bucket_arn" {
+  type = string
+}
+
+variable "events" {
+  type    = list(string)
+  default = ["s3:ObjectCreated:*"]
+}
+
+variable "prefix" {
+  type    = string
+  default = null
+}
+
+variable "suffix" {
+  type    = string
+  default = null
+}
---
+++ s3_lambda/main.tf
@@ -0,0 +1,20 @@
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = var.function_arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = var.bucket_arn
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = var.bucket_name
+
+  lambda_function {
+    lambda_function_arn = var.function_arn
+    events              = var.events
+    filter_prefix       = var.prefix
+    filter_suffix       = var.suffix
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket]
+}
