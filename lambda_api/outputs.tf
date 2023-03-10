8ff860c29b26c1c6ee16fc1deb881a52708dd59a
Nicolas Lagier <nicolas.lagier@gmail.com>
2023-03-10 08:33:54 +0800

    new output for lambda_api

--- lambda_api/outputs.tf
+++ lambda_api/outputs.tf
@@ -13,3 +13,13 @@ output "api_id" {
 output "api_root_resource_id" {
   value = aws_api_gateway_rest_api.api.root_resource_id
 }
+
+output "api" {
+  object = var.requires_key ? {
+    url           = module.aws_api_gateway_stage.stage.invoke_url
+    usage_plan_id = aws_api_gateway_usage_plan.usage_plan[0].id
+    } : {
+    url = module.aws_api_gateway_stage.stage.invoke_url
+  }
+  description = "same as looking at url and usage_plan_id separately but those often goes in pair. And it is a kind of convention to pass those 2 in this format across modules"
+}
