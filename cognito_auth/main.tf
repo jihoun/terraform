e7c2c76afef71c71fbd0133553d033e34690722b
Nicolas Lagier <nicolas.lagier@gmail.com>
2025-07-09 05:06:41 +0200

    Properly name cognito user pool

--- cognito_auth/main.tf
+++ cognito_auth/main.tf
@@ -70,7 +70,7 @@ resource "aws_iam_role" "cognito_role" {
       }
     ]
   })
-  path = "/service-role/${terraform.workspace}/authed"
+  path = "/service-role/${terraform.workspace}/authed/"
   tags = local.tags
   name = var.name
 }
@@ -86,7 +86,7 @@ resource "aws_cognito_identity_pool_roles_attachment" "main" {
 
 resource "aws_iam_policy" "cognito_authed" {
   count       = var.enabled ? 1 : 0
-  path        = "/service-role/${terraform.workspace}/authed"
+  path        = "/service-role/${terraform.workspace}/authed/"
   name_prefix = var.name
   tags        = local.tags
   policy = jsonencode({
