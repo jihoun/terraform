f04df902de31e59e4af89037e73c421275f6cb28
Nicolas Lagier <nicolas.lagier@gmail.com>
2026-02-23 20:07:24 +0800

    fix allow-headers

--- cors/main.tf
+++ cors/main.tf
@@ -36,6 +36,10 @@ resource "aws_api_gateway_method_response" "response_200" {
 
   depends_on = [aws_api_gateway_integration.options]
 }
+
+locals {
+  allow_headers = concat(["Content-Type", "Authorization", "X-Amz-Date", "X-Api-Key", "X-Amz-Security-Token"], var.allow_headers!=null ? var.allow_headers : [])
+}
 resource "aws_api_gateway_integration_response" "response_200" {
   count       = var.enabled ? 1 : 0
   rest_api_id = var.rest_api_id
@@ -44,7 +48,7 @@ resource "aws_api_gateway_integration_response" "response_200" {
   content_handling  = "CONVERT_TO_TEXT"
   status_code = 200
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token${join(",", var.allow_headers!=null ? var.allow_headers : [])}'"
+    "method.response.header.Access-Control-Allow-Headers" = "'${join(",", local.allow_headers)}'"
     "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'"
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
