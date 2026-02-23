d36e1e8cedec2fa23147a5cb14e8a66f0d8d2e16
Nicolas Lagier <nicolas.lagier@gmail.com>
2026-02-23 16:56:36 +0800

    Make sure that if cognito variable changes in lambda_api, the api is redeployed

--- lambda_api/main.tf
+++ lambda_api/main.tf
@@ -97,7 +97,7 @@ resource "aws_api_gateway_stage" "stage" {
   client_certificate_id = aws_api_gateway_client_certificate.certificate[0].id
   cache_cluster_enabled = local.cache ? true : null
   cache_cluster_size    = local.cache ? local.cache_size : null
-  depends_on            = [aws_cloudwatch_log_group.log]
+  depends_on            = [aws_cloudwatch_log_group.log, variable.cognito]
   tags                  = local.tags
 }
 
